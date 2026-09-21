8063b6988543aa794407d2cd9bdc58ed54a06e32
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-09-21 10:03:48 +0200

    fix: make header responsive and add feedback file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,11 @@ import Image from "next/image"
 const Header = () => {
   return (
     <header className='P-6'>
+      <div className='w-full h-32 sm:h-40 md:h-52 lg:h-64 object-cover'>
+         <img src="/logo.png" alt="header"  className='object-center'/>
+      </div>
       
-      <img src="/logo.png" alt="header"  className='w-full h-90 object-cover object-center'/>
+      
     </header>
   )
 }
